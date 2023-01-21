33940781913bb0f2981d07c1924e73964684a675
vincegab <vincegab@my.yorku.ca>
2023-01-21 12:25:54 -0500

    commit to branch2

--- Excercise/src/Recursion/Recur.java
+++ Excercise/src/Recursion/Recur.java
@@ -6,6 +6,7 @@ public class Recur {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Github");
+		System.out.println("Hello branch2");
 		
 		
 	}
